7a41d809940baf70030918ef767daf6eddd68cef
VladosAVM <dezxdc@yandex.ru>
2024-10-23 21:09:22 +0300

    Добавлен 2-ой урок. Изменено место расположения 1-го урока.

---
+++ LRN-CSharp-SimpleCode/001 - Lessons/002 - Структура проекта/002 - Структура проекта.cs
@@ -0,0 +1,27 @@
+﻿// Подключённые пространства имён
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * 1-ый вид многострочного коментария
+*/
+
+/*
+  2-ой вид многострочного коментария
+*/
+
+namespace _002___Структура_проекта // Пространство имён текущего проекта
+{
+    internal class Program // Класс проекта
+    {
+        // Базовый метод который является точкой входа в программу
+        static void Main(string[] args)
+        {
+            // Тело метода Main
+            Console.WriteLine("Описание структуры проекта");
+        }
+    }
+}
