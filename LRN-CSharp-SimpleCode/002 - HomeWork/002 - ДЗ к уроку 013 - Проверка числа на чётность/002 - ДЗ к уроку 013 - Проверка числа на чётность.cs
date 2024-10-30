26159cb4758e89c33bef22e7fa5563c5bb4b402b
VladosAVM <dezxdc@yandex.ru>
2024-10-30 16:51:54 +0300

    Добавлено решение домашнего задания 002 - ДЗ к уроку 013 - Проверка числа на чётность

---
+++ LRN-CSharp-SimpleCode/002 - HomeWork/002 - ДЗ к уроку 013 - Проверка числа на чётность/002 - ДЗ к уроку 013 - Проверка числа на чётность.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/* ДЗ к уроку 013 - Проверка числа на чётность
+ * 
+ * Разбор ДЗ: https://www.youtube.com/watch?v=bTSpiLPTVvQ
+ */
+
+namespace _002___ДЗ_к_уроку_013___Проверка_числа_на_чётность
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("- - - Проверка числа на чётность - - -" +
+                              "\nВведите число для проверкаи:");
+            int evenNum = int.Parse(Console.ReadLine());
+            if (evenNum % 2 == 0)
+            {
+                Console.WriteLine($"Число {evenNum} чётное");
+            }
+            else
+            {
+                Console.WriteLine($"Число {evenNum} НЕчётное");
+            }
+
+        }
+    }
+}
---
+++ LRN-CSharp-SimpleCode/002 - HomeWork/002 - ДЗ к уроку 013 - Проверка числа на чётность/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("002 - ДЗ к уроку 013 - Проверка числа на чётность")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("002 - ДЗ к уроку 013 - Проверка числа на чётность")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("94809af0-80a8-4033-86c0-46fdf736e7e3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
