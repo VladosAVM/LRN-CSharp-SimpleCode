e6e05fafea84393268fab0f8d444ffb26cdea70f
VladosAVM <dezxdc@yandex.ru>
2024-10-31 13:42:28 +0300

    Добавлено решение 003 - ДЗ к уроку 015 - Консольный калькулятор через If Else и Switch Case

---
+++ LRN-CSharp-SimpleCode/002 - HomeWork/003 - ДЗ к уроку 015 - Консольный калькулятор/003 - ДЗ к уроку 015 - Консольный калькулятор.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+/* ДЗ к уроку 015 - Консольный калькулятор
+ * 
+ * Разбор: https://www.youtube.com/watch?v=4aL0YVKt880
+ */
+
+namespace _003___ДЗ_к_уроку_015___Консольный_калькулятор
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Калькулятор через Switch Case
+            Console.WriteLine("Введите первое число:");
+            double numSC1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            double numSC2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Укажите арефметическую операцию: + - * /");
+            string mathOperationSC = Console.ReadLine();
+
+            switch (mathOperationSC)
+            {
+                case "+":
+                    Console.WriteLine($"{numSC1} + {numSC2} = " + (numSC1 + numSC2));
+                    break;
+                case "-":
+                    Console.WriteLine($"{numSC1} - {numSC2} = " + (numSC1 - numSC2));
+                    break;
+                case "*":
+                    Console.WriteLine($"{numSC1} * {numSC2} = " + (numSC1 * numSC2));
+                    break;
+                case "/":
+                    if (numSC1 != 0 && numSC2 != 0)
+                    {
+                        Console.WriteLine($"{numSC1} / {numSC2} = " + (numSC1 / numSC2));
+                    }
+                    else
+                    {
+                        Console.WriteLine("На ноль делить нельзя");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("\n!!! Выбрана неверная математическая операция !!!");
+                    break;
+            }
+
+            // Калькулятор через If Else
+            Console.WriteLine("Введите первое число:");
+            double numIE1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            double numIE2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Укажите арефметическую операцию: + - * /");
+            string mathOperationIE = Console.ReadLine();
+
+            if (mathOperationIE == "+")
+            {
+                Console.WriteLine($"{numIE1} + {numIE2} = " + (numIE1 + numIE2));
+            }
+            else if (mathOperationIE == "-")
+            {
+                Console.WriteLine($"{numIE1} - {numIE2} = " + (numIE1 - numIE2));
+            }
+            else if (mathOperationIE == "*")
+            {
+                Console.WriteLine($"{numIE1} * {numIE2} = " + (numIE1 * numIE2));
+            }
+            else if (mathOperationIE == "/")
+            {
+                if (numIE1 != 0 && numIE2 != 0)
+                {
+                    Console.WriteLine($"{numIE1} / {numIE2} = " + (numIE1 / numIE2));
+                }
+                else
+                {
+                    Console.WriteLine("На ноль делить нельзя");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n!!! Выбрана неверная математическая операция !!!");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ LRN-CSharp-SimpleCode/002 - HomeWork/003 - ДЗ к уроку 015 - Консольный калькулятор/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("003 - ДЗ к уроку 015 - Консольный калькулятор")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("003 - ДЗ к уроку 015 - Консольный калькулятор")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b716d654-ab0c-4fdc-be07-0ba36096d46b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
