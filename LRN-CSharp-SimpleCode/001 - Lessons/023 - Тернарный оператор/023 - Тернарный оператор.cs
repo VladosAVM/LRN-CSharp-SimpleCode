213994102e18ed475475792457c031ac0eed09f0
VladosAVM <dezxdc@yandex.ru>
2024-11-06 15:24:37 +0300

    Добавлен урок 023 - Тернарный оператор. Скорректировано ДЗ-005

---
+++ LRN-CSharp-SimpleCode/001 - Lessons/023 - Тернарный оператор/023 - Тернарный оператор.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/* Тернарный оператор
+ * 
+ * Источниик: https://www.youtube.com/watch?v=iQhVp4TGf64
+ */
+
+namespace _023___Тернарный_оператор
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* ?: - тернарный оператор 
+             * 
+             * Тернарный оператор используется, когда требуется
+             * простая проверка условий, чтобы избежать многословного
+             * блока If Else.
+             * 
+             * Синтаксис:
+             * переменная = первый_операнд (условие) ? второй_операнд (if_true) : третий_операнд (if_else)
+             */
+
+            bool accessAllowed;
+
+            string storedPassword = "qwert";
+            string enteredPassword = Console.ReadLine();
+
+            accessAllowed = enteredPassword == storedPassword ? true : false;
+            Console.WriteLine(accessAllowed);
+
+            int inputData = int.Parse(Console.ReadLine());
+            int outputData = inputData >= 0 ? inputData : 0;
+            Console.WriteLine(outputData); 
+        }
+    }
+}
