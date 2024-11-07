dc148fddb03210bea2d984dbea317eb11df3234f
VladosAVM <dezxdc@yandex.ru>
2024-11-07 11:42:48 +0300

    Добавлен урок 026 - Вывод массива - Перебор массива

---
+++ LRN-CSharp-SimpleCode/001 - Lessons/026 - Вывод массива - Перебор массива/026 - Вывод массива - Перебор массива.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/* Вывод массива - Перебор массива
+ * 
+ * Источник: https://www.youtube.com/watch?v=uRi-fKUYzUM
+ */
+
+namespace _026___Вывод_массива___Перебор_массива
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Для перебора массива используются циклы.
+             * Осуществлять перебор массива можно любым циклом,
+             * но наиболее удобными для этого будут циклы For и ForEach
+             */
+
+            int[] firstArray = { 10, 3, 2, 55 };
+
+            Console.WriteLine("Вывод массива в прямом порядке");
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                Console.Write($"{firstArray[i]} ");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("\nВывод массива в обратном порядке");
+            for (int i = firstArray.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{firstArray[i]} ");
+            }
+            Console.WriteLine("\n\n\n\n");
+        }
+    }
+}
