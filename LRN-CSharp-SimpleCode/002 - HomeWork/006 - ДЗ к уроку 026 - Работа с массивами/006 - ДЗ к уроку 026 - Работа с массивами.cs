4984f252f7200463561114ab27605ba4f0dfe6d0
VladosAVM <dezxdc@yandex.ru>
2024-11-07 12:21:58 +0300

    Добавлено решение 006 - ДЗ к уроку 026 - Работа с массивами

---
+++ LRN-CSharp-SimpleCode/002 - HomeWork/006 - ДЗ к уроку 026 - Работа с массивами/006 - ДЗ к уроку 026 - Работа с массивами.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+/* ДЗ к уроку 026 - Работа с массивами
+ * 
+ * Разбор: https://www.youtube.com/watch?v=7rcwQQT2txY
+ */
+
+namespace _006___ДЗ_к_уроку_026___Работа_с_массивами
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("- Заполнение массива с клавиатуры -");
+
+            int[] hwArray = new int[4];
+            for (int i = 0; i < hwArray.Length; i++)
+            {
+                Console.Write($"Введите данные для элемента массива под индексом {i} :");
+                hwArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\n- Вывод массива в обратном порядке -");
+            for (int i = hwArray.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{hwArray[i]} ");
+            }
+
+            Console.WriteLine("\n\n- Сумма чётных чисел в массиве -");
+            int evenSum = 0;
+
+            for (int i = 0; i < hwArray.Length; i++)
+            {
+                evenSum += hwArray[i] % 2 == 0 ? hwArray[i] : 0;
+            }
+            Console.WriteLine($"Сумма чёт. чисел = {evenSum}");
+
+            Console.WriteLine("\n- Поиск наименьшего числа в массиве -");
+            int minNum = hwArray[0];
+            for (int i = 0; i < hwArray.Length; i++)
+            {
+                minNum = minNum > hwArray[i] ? hwArray[i] : minNum;
+            }
+            Console.WriteLine($"Наименьшее число в массиве = {minNum}");
+
+
+
+
+        }
+    }
+}
