4a7785bf9fca17357d40918f792a58d1d777d778
VladosAVM <dezxdc@yandex.ru>
2024-11-18 10:01:46 +0300

    Добавлен урок 040 - Оператор объединения с null

---
+++ LRN-CSharp-SimpleCode/001 - Lessons/040 - Оператор объединения с null/040 - Оператор объединения с null.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+/* ?? - Оператор объединения с null
+ * 
+ * Источник: https://www.youtube.com/watch?v=mXh0ci6O6og
+ */
+
+namespace _040___Оператор_объединения_с_null
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* ?? - данный оператор позволяет присвоить новое
+             * дефолтное значение в переменные которые имеют
+             * значение null.
+             * 
+             * Данный оператор позволяет избежать использования
+             * конструкций типа if else для проверки и внесения
+             * изменений в значение переменной.
+             * Что позволяет сделать запись более краткой и понятной,
+             * а также избежать создания дополнительных переменных.
+             * 
+             * Пример БЕЗ использования ??:
+             *      string str = null;
+             *      string result;
+             *      Console.WriteLine(str ?? "Нет данных");
+             *
+             *      if (str == null)
+             *          result = "Нет данных";
+             *      else
+             *          result= str;
+             *
+             *      Console.WriteLine(result);
+             *      
+             */
+
+            // Пример с использованием ??:
+            string str = null;
+            string result = str ?? "Нет данных";
+            Console.WriteLine(result);
+
+            // Без создания дополнительной переменной result
+            Console.WriteLine(str ?? "Нет данных");
+
+            /* Также при помощи оператора ?? можно избегать
+             * появления ошибок в ходе выполнения программы.
+             * 
+             * Предположим к нам в программу приходят какието
+             * данные из-вне, и одними из таких данных будет null.
+             * При попытке както обработать подобные данные,
+             * с высокой долей вероятности мы получим ошибку
+             * NullReferenceException.
+             */
+
+            // Пример:
+            string testStr = null;
+            string testStrResult = str ?? string.Empty;
+            Console.WriteLine($"Длинна строки: {testStrResult.Length}"); // Без ошибки
+
+            Console.WriteLine($"Длинна строки: {testStr.Length}"); // С ошибкой NullReferenceException
+        }
+    }
+}
---
+++ LRN-CSharp-SimpleCode/001 - Lessons/040 - Оператор объединения с null/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("040 - Оператор объединения с null")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("040 - Оператор объединения с null")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e9a12a3d-91b3-4b78-b482-9370c2909a4c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
