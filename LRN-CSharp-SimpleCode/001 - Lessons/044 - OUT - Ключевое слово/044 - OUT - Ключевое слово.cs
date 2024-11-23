9f853ac264ad9ac26e1b7bed4580c4c2120bf180
VladosAVM <dezxdc@yandex.ru>
2024-11-23 17:28:54 +0300

    Добавлен урок 044 - OUT - Ключевое слово

---
+++ LRN-CSharp-SimpleCode/001 - Lessons/044 - OUT - Ключевое слово/044 - OUT - Ключевое слово.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Runtime.InteropServices.ComTypes;
+
+/* OUT - Ключевое слово
+ * 
+ * Источник: https://www.youtube.com/watch?v=R9i0XFWkFps
+ */
+
+namespace _044___OUT___Ключевое_слово
+{
+    internal class Program
+    {
+        static void TestRef(ref int value)
+        {
+            value++;
+        }
+        static void TestOut(out int value)
+        {
+            // value++; // Ошибка, переменной не присвоено значение
+            value = 10;
+        }
+        static void Main(string[] args)
+        {
+            /* out - ключевое слово которое также как и ref позволяет
+             * передавать переменные по ссылке.
+             * 
+             * ref - обязывает присвивать значение переменной ДО передачи
+             * в метод, но НЕ обязывает присваивать значение после
+             * передачи в метод (внутри метода).
+             * 
+             * out - НЕ обязывает присваивать значение переменной ДО передачи
+             * в метод, но обязывает присваивать значение после передачи
+             * в методв (внутри метода).
+             */
+
+            // int testRefInt; // Ошибка, переменной не присвоено значение
+            int testRefInt = 10;
+            TestRef(ref testRefInt);
+            Console.WriteLine($"Переменная testRefInt = {testRefInt}");
+
+            // TestRef(ref int x); // Ошибка
+                                   // Переменную с ключевым словом ref нельзя
+                                   // определить прямо в параметрах вызова метода
+
+            int testOutInt;  // Переменной передоваемой с помощью out
+                             // не обязательно присваивать значение
+            testOutInt = 10; // однако можно и присвоить значение перед
+                             // передачей в метод, но внутри метода по
+                             // прежнему будет ОБЯЗАТЕЛЬНЫМ присвоение
+                             // нового значения
+            TestOut(out testOutInt);
+            Console.WriteLine($"Переменная testOutInt = {testOutInt}");
+
+            TestOut(out int x); // Переменную с ключевым словом out можно
+                                // определить прямо в параметрах вызова метода
+            Console.WriteLine($"Переменная x = {x}");
+
+            // Пример применения out
+            Console.Write("\nВведите данные: ");
+
+            string str = Console.ReadLine();
+            int.TryParse(str, out int result);
+            Console.WriteLine($"result = {result}");
+        }
+    }
+}
---
+++ LRN-CSharp-SimpleCode/001 - Lessons/044 - OUT - Ключевое слово/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("044 - OUT - Ключевое слово")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("044 - OUT - Ключевое слово")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("79bfa3ad-3d4f-4fae-a60d-79d5ec98df4c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
