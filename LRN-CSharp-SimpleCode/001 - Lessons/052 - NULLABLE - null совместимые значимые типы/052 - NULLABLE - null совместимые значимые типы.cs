07ddf5ae5f8c7ea91cced485e00646837fb39031
VladosAVM <dezxdc@yandex.ru>
2024-12-02 22:47:05 +0300

    Добавлен урок 052 - NULLABLE - null совместимые значимые типы

---
+++ LRN-CSharp-SimpleCode/001 - Lessons/052 - NULLABLE - null совместимые значимые типы/052 - NULLABLE - null совместимые значимые типы.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+/* NULLABLE - null совместимые значимые типы
+ * 
+ * Источник: https://www.youtube.com/watch?v=jhz3Ltc09m4
+  */
+
+namespace _052___NULLABLE___null_совместимые_значимые_типы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* null можно использовать со значимыми типами данных, однако поведение
+             * переменной будет отличаться от поведения ссылочных типов данных.
+             * 
+             * 
+             * 1. Т.к. у значимых типов нет ссылки, то и null не будет удалять ссылку,
+             * а будет просто говорить об отсутствии значения в переменной.
+             * 
+             * 2. После определения такой переменной у неё появляются 2-а дополнительных
+             * поля и метод:
+             *      .HasValue - проверяет наличие значения в переменной true\false
+             *      .Value    - выдаёт значение которое присвоено переменной. Используется
+             *                  в случае когда с переменной совершаются какието действия.
+             *                  В случае если в переменной null а мы вытаетесь напрямую оперировать
+             *                  значением переменной то будет ошибка.
+             *      .GetValueOrDefault() - выводит текущее значение или дефолтное если
+             *                             текущее null, также дефолтное значение можно
+             *                             задать самостоятельно
+             */
+
+            int? i = null;
+            Console.WriteLine(i);                      // 
+            Console.WriteLine(i == null);              // True
+            Console.WriteLine(i.HasValue);             // False
+            Console.WriteLine(i.GetValueOrDefault());  // 0
+            Console.WriteLine(i.GetValueOrDefault(4)); // 4
+            Console.WriteLine(i ?? 98);                // 98
+            Console.WriteLine(i + 1);                  // 
+            //Console.WriteLine(i.Value);              // InvalidOperationException
+            i = 4;
+            Console.WriteLine(i.HasValue);             // True
+            Console.WriteLine(i.Value);                // Значение которое присвоено переменной
+
+            /* При работе с nullable переменными стоит учитывать что они могут
+             * оказать влияние на дальнейший код.
+             * 
+             * Например при осуществлении арефметических операций над двумя переменными одна из
+             * котороых может быть nullable, стоит помнить и проверять переменную в которую будет
+             * записан результат операции.
+             * 
+             * Пример:
+             */
+
+            int x = 7;
+            int? y = null;
+            int? result = x + y;
+            Console.WriteLine(result); // 
+            Console.WriteLine(x == y); // false
+            Console.WriteLine(x >= y); // false
+            Console.WriteLine(x < y);  // false
+
+            y = 7;
+            result = x + y;
+            Console.WriteLine(result); // 14
+            Console.WriteLine(x == y); // true
+            Console.WriteLine(x >= y); // true
+            Console.WriteLine(x < y);  // false
+        }
+    }
+}
