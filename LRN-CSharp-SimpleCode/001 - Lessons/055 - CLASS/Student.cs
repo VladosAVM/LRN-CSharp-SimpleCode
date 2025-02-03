dfba6aac96e8bbb125c2c1a53decc676313332b5
VladosAVM <dezxdc@gmail.com>
2025-02-03 18:06:02 +0300

    Добавлен урок 055- CLASS

---
+++ LRN-CSharp-SimpleCode/001 - Lessons/055 - CLASS/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _055___CLASS
+{
+    /// <summary>
+    /// Класс описывающий свойства студента
+    /// </summary>
+    class Student
+    {
+        public Guid id;             // Уникалный идентификационный номер
+        public string firstName;    // Имя
+        public string lastName;     // Фамилия
+        public string middleName;   // Отчество
+        public int age;             // Возрост
+        public string group;        // Группа
+    }
+}
