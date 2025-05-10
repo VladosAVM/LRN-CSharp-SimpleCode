e3c61bf5f5ed392708aa80f50b2fc10286544e2c
VladosAVM <dezxdc@gmail.com>
2025-05-10 13:13:45 +0300

    Скорректировано описание урока 061 - THIS - Ключевое слово

--- LRN-CSharp-SimpleCode/999 - Test/001 - TEST/TEST.cs
+++ LRN-CSharp-SimpleCode/999 - Test/001 - TEST/TEST.cs
@@ -3,18 +3,12 @@ using System;
 
 namespace _001___TEST
 {
-
     internal class Program
     {
-        static void ASD(ref int? x)
-        {
-
-        }
 
         static void Main(string[] args)
         {
 
-
         }
-    } 
+    }
 }
